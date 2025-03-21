81c86336baaebbbee7128c3c9eb14c857447ac5b
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2025-03-20 20:20:28 -0500

    Generate Test model migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_17_014233) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_21_010433) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tests", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.integer "number_correct"
+    t.integer "number_incorrect"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tests_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +36,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_17_014233) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tests", "users"
 end
